1bbfda003f0a8301afc6d63f44e5a87b46d4d425
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-07-14 16:36:19 -0400

    initial commit

---
+++ generateMarkdown.js
@@ -0,0 +1,40 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+
+  //in here we will want a conditional statement to check if there is a license (wether there is a license
+  //should be one of the array questions) If license is there - return the license.  If not there return and 
+  // empty string. 
+
+  // write a template literal here like this to return it - return `![text to be displayed(the link itself)]`
+// use the second link that he slacks out here.....for the badge 
+}
+
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+
+  //in here do the same pattern you used above with the conditional statement
+  //this is where you will be returning the license link
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  //same pattern as above
+  
+  //return `## License This repository has the ${Licesnse} License`
+  //
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title} 
+  
+
+`;
+}
+//in the generateMarkdown you will want things like the ## Table of contents, ## description, anything else 
+//rendering all the other things here
+module.exports = generateMarkdown;
---
+++ index.js
@@ -0,0 +1,39 @@
+// TODO: Include packages needed for this application
+// Will need fs to write the file and create readme, will need inquirere
+//will need path to join the path, will need the generatemarkdown.js file - use require for that
+
+
+// TODO: Create an array of questions for user input
+// const questions = [
+//     type: "input"
+//     name: "github"
+//     message: "what is your github username?"
+// ];
+//the questions should be there in the readme.  build the array with those questions
+const questions = [];
+
+
+
+// TODO: Create a function to write README file
+//use fs that we imported up top to write the file, will want to use path to join the 
+//file.  reference activity 13 to help with this. 
+function writeToFile(fileName, data) {}
+
+
+
+
+// TODO: Create a function to initialize app
+//this is where you want to actually use inquirer and prompt to show the questions,
+//take in the responses and write the responses to the file
+//reference activity 20.  in the init function you will also want to call the write to file function
+//so that the answers go to the write to file funciton. 
+//inside the write to file function you will want the file name, and to call your generatemarkdown function
+//and pass in the response that you got from the questions
+function init() {}
+
+
+
+
+// Function call to initialize app
+init();
+
